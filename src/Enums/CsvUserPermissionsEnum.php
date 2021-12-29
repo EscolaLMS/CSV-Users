216d8b1ff2c146b58a7621f2e409c2bfdd1ef343
KrzysztofDziedziechEscolasoft <96292232+KrzysztofDziedziechEscolasoft@users.noreply.github.com>
2021-12-29 12:34:45 +0100

    #94 - permissions renamed (#11)
    * #94 - permissions renamed
    
    * Update src/Enums/CsvUserPermissionsEnum.php
    
    Co-authored-by: KD <do>
    Co-authored-by: Mateusz Qunabu <mateusz@qunabu.com>

--- src/Enums/CsvUserPermissionsEnum.php
+++ src/Enums/CsvUserPermissionsEnum.php
@@ -6,6 +6,6 @@ use EscolaLms\Core\Enums\BasicEnum;
 
 class CsvUserPermissionsEnum extends BasicEnum
 {
-    const CSV_USERS_EXPORT = 'csv users export';
-    const CSV_USERS_IMPORT = 'csv users import';
+    const CSV_USERS_EXPORT = 'csv-users_export';
+    const CSV_USERS_IMPORT = 'csv-users_import';
 }
