6afd4b195fc93ec1eb225cc5858d34758105893c
Maciej Rymarz <59456825+mako321@users.noreply.github.com>
2022-02-01 11:38:28 +0100

    Fix validation rule (#13)

--- src/Http/Requests/ImportUsersFromCsvAPIRequest.php
+++ src/Http/Requests/ImportUsersFromCsvAPIRequest.php
@@ -15,7 +15,7 @@ class ImportUsersFromCsvAPIRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'file' => ['required', 'mimes:csv'],
+            'file' => ['required', 'mimes:csv,txt'],
             'return_url' => ['required', 'string'],
         ];
     }
