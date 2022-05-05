aedbb4dae863aa234fc1efe101b5e591ca78f63a
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2022-05-05 14:31:43 +0200

    Add route from CORE

--- src/routes.php
+++ src/routes.php
@@ -2,9 +2,9 @@
 
 // admin endpoints
 use EscolaLms\CsvUsers\Http\Controllers\CsvUserAPIController;
-use Illuminate\Support\Facades\Route;
+use EscolaLms\Core\Http\Facades\Route;
 
-Route::group(['middleware' => ['auth:api'], 'prefix' => 'api/admin/csv'], function () {
+Route::group(['middleware' => Route::apply(['auth:api']), 'prefix' => 'api/admin/csv'], function () {
     Route::get('users', [CsvUserAPIController::class, 'export']);
     Route::post('users', [CsvUserAPIController::class, 'import']);
 });
