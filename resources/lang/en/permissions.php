559eb06f25d4c9196b166e215a0c2c937b7f07d5
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-11-18 15:44:05 +0100

    Add translations

---
+++ resources/lang/en/permissions.php
@@ -0,0 +1,8 @@
+<?php
+
+use EscolaLms\CsvUsers\Enums\CsvUserPermissionsEnum;
+
+return [
+    CsvUserPermissionsEnum::CSV_USERS_EXPORT => 'Export csv users',
+    CsvUserPermissionsEnum::CSV_USERS_IMPORT => 'Import csv users',
+];
---
+++ resources/lang/pl/permissions.php
@@ -0,0 +1,8 @@
+<?php
+
+use EscolaLms\CsvUsers\Enums\CsvUserPermissionsEnum;
+
+return [
+    CsvUserPermissionsEnum::CSV_USERS_EXPORT => 'Eksportuj użytkowników do csv',
+    CsvUserPermissionsEnum::CSV_USERS_IMPORT => 'Importuj użytkowników z csv',
+];
