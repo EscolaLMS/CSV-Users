d93d044d4d4b07c7510e06281c5fb533a74155eb
Dawid Miklas <dawid.miklas@escolasoft.com>
2023-10-23 15:07:02 +0200

    Disabled heading formatting, so it doesn't skip non snake case columns name

--- src/EscolaLmsCsvUsersServiceProvider.php
+++ src/EscolaLmsCsvUsersServiceProvider.php
@@ -7,6 +7,7 @@ use EscolaLms\CsvUsers\Services\Contracts\CsvUserServiceContract;
 use EscolaLms\CsvUsers\Services\CsvUserGroupService;
 use EscolaLms\CsvUsers\Services\CsvUserService;
 use Illuminate\Support\ServiceProvider;
+use Maatwebsite\Excel\Imports\HeadingRowFormatter;
 
 /**
  * SWAGGER_VERSION
@@ -22,6 +23,8 @@ class EscolaLmsCsvUsersServiceProvider extends ServiceProvider
     {
         $this->loadRoutesFrom(__DIR__.'/routes.php');
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'csv-users');
+
+        HeadingRowFormatter::default('none');
     }
 
     public function register()
