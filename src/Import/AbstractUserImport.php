16a5069256a64adf9ca022e68afeddb4a9226b12
Maciej Rymarz <59456825+mako321@users.noreply.github.com>
2023-10-23 11:09:54 +0200

    Add path_avatar and fix validation (#30)
    * Add path_avatar and fix validation
    
    * WIP

--- src/Import/AbstractUserImport.php
+++ src/Import/AbstractUserImport.php
@@ -35,19 +35,21 @@ abstract class AbstractUserImport
     {
         $arrayKeys = ['roles', 'permissions', 'groups'];
 
-        return $data->map(function ($item) use ($arrayKeys) {
+        return $data->map(function (Collection $item) use ($arrayKeys) {
             foreach ($arrayKeys as $key) {
-                $item->put($key, json_decode($item->get($key, '[]'), true));
+                $item->put($key, json_decode($item->get($key), true) ?? []);
             }
 
             $item->put('is_active', $item->get('is_active') ?? false);
             $item->forget('created_at');
             $item->forget('updated_at');
 
-            $item->put('path_avatar', $item->get('path_avatar') !== null && Storage::exists($item->get('path_avatar'))
-                ? $item->get('path_avatar')
-                : null
-            );
+            if ($item->has('path_avatar')) {
+                $item->put('path_avatar', $item->get('path_avatar') !== null && Storage::exists($item->get('path_avatar'))
+                    ? $item->get('path_avatar')
+                    : null
+                );
+            }
 
             if ($item->get('password')) {
                 $item->put('password', $item->get('password'));
